e84adb4c50cf4b0fac4df6dcc909376a2be9f50a
jkonury <jkonury32@gmail.com>
2016-04-11 15:46:27 +0900

    first init

---
+++ src/main/scala-2.11/Sample.scala
@@ -0,0 +1,18 @@
+
+object Sample {
+  implicit class ToAge(val n: Int) extends AnyVal {
+    def years(x: old.type): Age = Age(n)
+    def year(x: old.type): Age = years(x)
+  }
+
+  case object old
+  case class Age(n: Int) {
+    override def toString: String = s"$n ${if (n > 1) "years" else "year"} old"
+  }
+
+  def main(args: Array[String]) {
+    println(Age(10))
+    println(10 years old)
+
+  }
+}
